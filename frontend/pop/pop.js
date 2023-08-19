2a0e1e39378e0c2df456bba8c52e02ce21d3b28a
BHARATHI <88837161+BHARATHI-GIT-HUB@users.noreply.github.com>
2023-08-19 22:13:47 +0530

    final commit

--- frontend/pop/pop.js
+++ frontend/pop/pop.js
@@ -1,5 +1,4 @@
 document.getElementById("getUrl").onclick = getCurrUrl;
-document.getElementById("fetchData").onclick = fetchData;
 
 function getCurrUrl() {
   chrome.tabs.query(
@@ -9,36 +8,7 @@ function getCurrUrl() {
     },
     function (tabs) {
       const tab = tabs[0];
-
-      const xhr = new XMLHttpRequest();
-
-      xhr.open("GET", tab.url, true);
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          const parser = new DOMParser();
-          const htmlDoc = parser.parseFromString(xhr.responseText, "text/html");
-          alert(tab.url.split("/")[2]);
-
-          if (tab.url.split("/")[2] != "www.amazon.in") {
-            document.getElementById("title").innerHTML =
-              "Can't scrape this website";
-            document.getElementById("load").id = "noload";
-            document.getElementById("fetchData").id = "none";
-            document.getElementById("getProduct").id = "none";
-          }
-
-          const title = htmlDoc.querySelector("title").textContent;
-          const name = title.split(",");
-
-          document.getElementById("title").innerHTML = name[0];
-          document.getElementById("load").id = "noload";
-          document.getElementById("getProduct").id = "none";
-        } else {
-          document.getElementById("none").id = "load";
-        }
-      };
-      xhr.send();
-      alert(tab.url);
+      alert(tab.title);
     }
   );
 }
